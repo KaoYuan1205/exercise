c1a39ea3e5520b33fe55a9f144223b02f138c831
KaoYuan1205 <s1143049@gm.ncue.edu.tw>
2024-12-03 21:52:00 +0800

    Update and rename 1_🌍_Interactive_Map.py to 1_🌍北部海巡署AED點位.py

--- pages/1_🌍北部海巡署AED點位.py
+++ pages/1_🌍北部海巡署AED點位.py
@@ -2,7 +2,6 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import geopandas as gpd
 
-# 側邊欄內容
 markdown = """
 A Streamlit map template
 <https://github.com/opengeos/streamlit-map-template>
@@ -12,23 +11,18 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-# 主頁標題
 st.title("Interactive Map")
 
-# 分欄
 col1, col2 = st.columns([4, 1])
 options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
 
 with col2:
-    # 底圖選項
     basemap = st.selectbox("Select a basemap:", options, index)
 
-# 加載 GeoJSON 數據
 TW_fishing = gpd.read_file('https://github.com/KaoYuan1205/homework1127/raw/refs/heads/main/f202311305757.geojson')
 
 with col1:
-    # 創建地圖
     m = leafmap.Map(
         center=[23.7652, 120.4980],
         zoom=8,
@@ -37,15 +31,13 @@ with col1:
         draw_export=True,
         minimap_control=True,
     )
-    # 添加底圖
     m.add_basemap(basemap)
 
-    # 添加 GeoJSON 點數據
     m.add_points_from_xy(
         TW_fishing,
-        x='座標(WGS84)X',  # 替換為數據中 X 坐標欄位名
-        y='座標(WGS84)Y',  # 替換為數據中 Y 坐標欄位名
-        layer_name='全台開放釣點'
+        x='座標(WGS84)X',
+        y='座標(WGS84)Y', 
+        layer_name='北部海巡署AED點位'
     )
 
     # 顯示地圖
