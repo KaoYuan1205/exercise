f364c5e657a0903fdc3b0fa550c77f664ae2bc3f
KaoYuan1205 <s1143049@gm.ncue.edu.tw>
2024-12-03 21:40:32 +0800

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+
 
 markdown = """
 A Streamlit map template
-<https://github.com/KaoYuan1205/homework1127/raw/refs/heads/main/f202311305757.geojson>
+<https://github.com/opengeos/streamlit-map-template>
 """
 
 st.sidebar.title("About")
@@ -17,13 +21,29 @@ st.title("Interactive Map")
 col1, col2 = st.columns([4, 1])
 options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
-
 with col2:
 
     basemap = st.selectbox("Select a basemap:", options, index)
 
+TW_fishing=gpd.read_file('https://github.com/KaoYuan1205/homework1127/raw/refs/heads/main/f202311305757.geojson')
 
-with col1:
+with st.expander("See source code"):
+    
+    with st.echo():
+        with col1:
+
+            m = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+            )
+            m.add_basemap(basemap)
+            m.add_points_from_xy(TW_fishing,
+                          x='XPOS',
+                          y='YPOS',
+                          spin=True,
+                          add_legend=True,
+                          layer_name='全台開放釣點')
+            m.to_streamlit(height=700)
+st.dataframe(TW_fishing)
 
     m = leafmap.Map(
         locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
